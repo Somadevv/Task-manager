de5a3942efb6f367ccb1aced7ae8f658d85ba2c2
Josh <josh@insitesdigital.com>
2023-06-21 19:37:05 +0100

    Added neccesary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskDialogComponent } from './components/task-dialog/task-dialog.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent,
+    TaskListComponent,
+    TaskDialogComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
